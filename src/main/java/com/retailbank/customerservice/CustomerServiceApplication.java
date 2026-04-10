26f5bf7799c61972f254f427c7372024182274e6
AbhijitDeepakJadhav <abhijeetjadhav2016@gmail.com>
2026-04-10 11:34:12 +0530

    Issue-5: Implement Customer Repository and Basic custom Database Operations

--- src/main/java/com/retailbank/customerservice/CustomerServiceApplication.java
+++ src/main/java/com/retailbank/customerservice/CustomerServiceApplication.java
@@ -1,5 +1,9 @@
 package com.retailbank.customerservice;
 
+import com.retailbank.customerservice.entity.Customer;
+import com.retailbank.customerservice.entity.CustomerStatus;
+import com.retailbank.customerservice.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,8 +12,30 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class CustomerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
-	}
+	@Autowired
+	CustomerRepository repository;
 
+	public static void main(String[] args) {
+		var context = SpringApplication.run(CustomerServiceApplication.class, args);
+//
+//		CustomerRepository repository = context.getBean(CustomerRepository.class);
+//
+////		Customer cs = new Customer();
+////		cs.setFirstName("Abhi");
+////		cs.setLastName("Jadhav");
+////		cs.setEmail("abhijeetjadhav2016@gmail.com");
+////		cs.setPhone("9146268993");
+////		cs.setStatus(CustomerStatus.INITIATED);
+////
+////		repository.save(cs);
+//
+////		System.out.println("Customer saved from main()");
+//
+//		System.out.println("\nDo we have abhi? " + (repository.existsByEmail("abhijeetjadhav2016@gmail.com")?"YES":"NO"));
+//
+//		Customer customer = repository.findByEmail("abhijeetjadhav2016@gmail.com");
+//
+//		System.out.println(customer);
+
+	}
 }
--- src/main/java/com/retailbank/customerservice/repository/CustomerRepository.java
+++ src/main/java/com/retailbank/customerservice/repository/CustomerRepository.java
@@ -1,10 +1,17 @@
 package com.retailbank.customerservice.repository;
 
+import com.retailbank.customerservice.CustomerServiceApplication;
 import com.retailbank.customerservice.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+
+    public Customer findByEmail(String email);
+
+    public boolean existsByEmail(String email);
 
 }
